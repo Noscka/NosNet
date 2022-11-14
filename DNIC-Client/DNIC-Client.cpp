e7d7cba18b7d03d8312fe98b8d81a5a4a1e39bff
Noscka <Adam882200@gmail.com>
2022-11-14 20:10:58 +0000

    Added NosStdLib InitializeModifiers to both files

--- DNIC-Client/DNIC-Client.cpp
+++ DNIC-Client/DNIC-Client.cpp
@@ -1,6 +1,15 @@
+#include <NosStdLib/Global.hpp>
+
 #include <iostream>
+#include <conio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    NosStdLib::Global::Console::InitializeModifiers::EnableUnicode();
+    NosStdLib::Global::Console::InitializeModifiers::EnableANSI();
+
+    wprintf(L"Some test text\n");
+
+    wprintf(L"Press any button to continue"); _getch();
+    return 0;
 }
