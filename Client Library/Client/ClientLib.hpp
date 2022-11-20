8c64ae4f27bd5f40a6bd55ee9b7f29d3da6a5ef7
Noscka <Adam882200@gmail.com>
2022-11-20 19:51:58 +0000

    doing a bit of client

--- Client Library/Client/ClientLib.hpp
+++ Client Library/Client/ClientLib.hpp
@@ -1,8 +1,19 @@
 #ifndef _CLIENT_LIBRARY_NOSNET_HPP_
 #define _CLIENT_LIBRARY_NOSNET_HPP_
 
+#include <string>
+
 namespace ClientLib
 {
+    namespace Constants
+    {
+        /* Default Connection info */
+        inline const std::string DefaultPort = "58233";
+        inline const std::string DefaultHostname = "localhost";
 
+        /* Connection info for update service */
+        inline const std::string UpdateServiceHostName = DefaultHostname;
+        inline const std::string UpdateServicePort = DefaultPort;
+    }
 }
 #endif
--- DNIC-Client/DNIC-Client.cpp
+++ DNIC-Client/DNIC-Client.cpp
@@ -1,14 +1,42 @@
 #include <NosStdLib/Global.hpp>
 
+#include <boost/asio.hpp>
+
 #include <iostream>
 #include <conio.h>
+
+#include ""
 
 int main()
 {
     NosStdLib::Global::Console::InitializeModifiers::EnableUnicode();
     NosStdLib::Global::Console::InitializeModifiers::EnableANSI();
 
-    wprintf(L"Some test text\n");
+    boost::asio::io_context io_context;
+
+    boost::asio::ip::tcp::socket socket(io_context);
+
+    try
+    {
+        wprintf(L"Type in hostname: ");
+        std::string HostName;
+        std::getline(std::cin, HostName);
+
+        /*
+        Connects to the function using `resolver` which resolves the address e.g. (Noscka.com -> 123.123.123.123)
+        Host - Hostname/Ip address
+        Service - Service(Hostname for ports)/Port number
+        */
+        boost::asio::connect(socket, boost::asio::ip::tcp::resolver(io_context).resolve(HostName, ClientLib::Constants::DefaultPort));
+
+        wprintf(L"Connected to server\n");
+
+        
+    }
+    catch (std::exception& e)
+    {
+        std::wcerr << e.what() << std::endl;
+    }
 
     wprintf(L"Press any button to continue"); _getch();
     return 0;
